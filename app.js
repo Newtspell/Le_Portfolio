7d7513b5cac249857fd2f0a8faf2ef2765406c69
Newtspell <contactnewtspell.tech>
2021-05-05 20:33:29 +0200

    Section présentation et Medias queries

--- app.js
+++ app.js
@@ -20,3 +20,32 @@ if(window.matchMedia('(max-width: 1300px)')) {
     })
 
 }
+
+// Animation écriture
+
+const txtAnim = document.querySelector('.txt-animation');
+
+let typewriter = new Typewriter(txtAnim,  {
+    loop: false,
+    deleteSpeed: 20
+})
+
+typewriter 
+.pauseFor(1800)
+.changeDelay(20)
+.typeString('Moi c\'est Newt Spell')
+.pauseFor(300)
+.typeString('<strong>, Développeur Full-Stack</strong> !')
+.pauseFor(1000)
+.deleteChars(13)
+.typeString('<span style="color: #27ae60;"> CSS</span> !')
+.pauseFor(1000)
+.deleteChars(5)
+.typeString('<span style="color: #EA39ff;"> PhP</span> !')
+.pauseFor(1000)
+.deleteChars(5)
+.typeString('<span style="color: midnightblue;"> React</span> !')
+.pauseFor(1000)
+.deleteChars(8)
+.typeString('<span style="color: #ff6910;"> JavaScript</span> !')
+.start()
