d7815d1273c194856703eb6c444e95b6124be7a2
Newtspell <contactnewtspell.tech>
2021-05-31 00:55:44 +0200

    Animations inputs

--- app.js
+++ app.js
@@ -49,3 +49,22 @@ typewriter
 .deleteChars(8)
 .typeString('<span style="color: #ff6910;"> JavaScript</span> !')
 .start()
+
+
+// Animation Contact
+
+const input_fields = document.querySelectorAll('input');
+
+for(let i = 0; i < input_fields.length; i++) {
+
+    let field = input_fields[i];
+
+    field.addEventListener('input', (e) => {
+        if(e.target.value !== ''){
+            e.target.parentNode.classList.add('animation')
+        } else if (e.target.value == ''){
+            e.target.parentNode.classList.remove('animation')
+        }
+    })
+
+}
